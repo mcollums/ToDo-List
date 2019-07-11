9f3549ffd3d3f21a55288a5c2e7756ede77a33c5
mcollums <collumsmichelle@gmail.com>
2019-07-11 16:08:30 -0500

    Created server file

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,11 +1,14 @@
 console.log("Connected to the controller JS");
 var express = require("express");
+
 var todosModel = require("../model/todo.js");
+
 var router = express.Router();
 
 router.get("/", function(req, res){
     todosModel.selectAll(function(data){
         console.log(data);
+
     });
 });
 
