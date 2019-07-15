becc8658ee131a26618f3affc3a015acd451a2cb
mcollums <collumsmichelle@gmail.com>
2019-07-14 19:19:21 -0500

    updated connection for JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 //requiring the mysql npm
 var mysql = require("mysql");
 
-//credentials for the connection to the local mysql database
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "@Torpedoes1",
-    database: "todo_db"
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+//credentials for the connection to the local mysql database
+    var connection = mysql.createConnection({
+        host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "f8fbdvlafn5eibbe",
+        password: "b9m084knd02wnp58",
+        database: "ey0m09tlhzp9m2fr"
+    });
+}
 //creating connection to the local db
 connection.connect(function(err){
     if(err){
